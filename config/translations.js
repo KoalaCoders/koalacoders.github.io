fde086931935987443743ba742578031529eee79
Ihor Motornyi <igormotorny1997@gmail.com>
2016-11-26 05:19:03 +0200

    Fixed some texts. Change header. Fixed team layout

---
+++ config/translations.js
@@ -0,0 +1,59 @@
+window.translations = {
+  header: {
+    main: {
+      en: 'Main',
+      uk: 'Головна',
+      ru: 'Главная'
+    },
+    lections: {
+      en: 'Lections',
+      uk: 'Лекції',
+      ru: 'Лекции'
+    }
+  },
+  about: {
+    title: {
+      en: 'About',
+      uk: 'Хто ми',
+      ru: 'Кто мы'
+    },
+    history: {
+      en: 'We are group of javascript developers from KPI. Our main mission motivate it\'s students to self education. We want to share knowlage and study together.',
+      uk: 'Ми группа javascript девелоперів тактак чи інакше зв\'язаних з КПІ. В першу чергу наша ціль це пробудити інтерес студентів до самостійного навчання. Ми хочемо ділитися своїми знаннями та розвиватися разом з вами.',
+      ru: 'Мы группа javascript разработчиков так или иначе связаных с КПИ. В первую очередь наша цель - пробудить интерес студентов к самостоятельному обучению. Мы хотим делиться своими знаниями и развиваться вместе с вами.'
+    }
+  },
+  team: {
+    title: {
+      en: 'Team',
+      uk: 'Команда',
+      ru: 'Команда'
+    },
+    vuchkan: {
+      en: 'Volodimir Vuchkan',
+      uk: 'Володимир Вучкан',
+      ru: 'Владимир Вучкан'
+    },
+    klinec: {
+      en: 'Viktor Klinec',
+      uk: 'Віктор Клинець',
+      ru: 'Виктор Клинец'
+    },
+    motornyi: {
+      en: 'Ihor Motornyi',
+      uk: 'Ігор Моторний',
+      ru: 'Игорь Моторный'
+    },
+    rudnytskykh: {
+      en: 'Dmytro Rudnytskykh',
+      uk: 'Дмитро Рудницьких',
+      ru: 'Дмитрий Рудницких'
+    },
+    stetsenko: {
+      en: 'Ivan Stetsenko',
+      uk: 'Іван Стеценко',
+      ru: 'Иван Стеценко'
+    },
+
+  }
+};
--- index.js
+++ index.js
@@ -1,68 +1,3 @@
-window.translations = {
-  header: {
-    about: {
-      en: 'About',
-      uk: 'Хто ми',
-      ru: 'Кто мы'
-    },
-    team: {
-      en: 'Team',
-      uk: 'Команда',
-      ru: 'Команда'
-    },
-    lections: {
-      en: 'Lections',
-      uk: 'Лекції',
-      ru: 'Лекции'
-    }
-  },
-  about: {
-    title: {
-      en: 'About',
-      uk: 'Хто ми',
-      ru: 'Кто мы'
-    },
-    history: {
-      en: 'Our awesome story',
-      uk: 'Чудова історія коали',
-      ru: 'Наша мега офигенная история'
-    }
-  },
-  team: {
-    title: {
-      en: 'Team',
-      uk: 'Команда',
-      ru: 'Команда'
-    },
-    vuchkan: {
-      en: 'Volodimir Vuchkan',
-      uk: 'Володимир Вучкан',
-      ru: 'Вадимир Вучкан'
-    },
-    klinec: {
-      en: 'Viktor Klinec',
-      uk: 'Віктор Клинець',
-      ru: 'Виктор Клинец'
-    },
-    motornyi: {
-      en: 'Ihor Motornyi',
-      uk: 'Ігор Моторний',
-      ru: 'Игорь Моторный'
-    },
-    rudnytskykh: {
-      en: 'Dmytro Rudnytskykh',
-      uk: 'Дмитро Рудницьких',
-      ru: 'Дмитрий Рудницких'
-    },
-    stetsenko: {
-      en: 'Ivan Stetsenko',
-      uk: 'Іван Стеценко',
-      ru: 'Иван Стеценко'
-    },
-
-  }
-};
-
 class KoalaTranslate {
 
   constructor(translations = {}, currentLanguage = 'en') {
@@ -96,13 +31,16 @@ class KoalaTranslate {
 }
 
 window.addEventListener('load', () => {
-  let translate = new KoalaTranslate(window.translations);
+  const translate = new KoalaTranslate(window.translations, 'uk');
+
   document.querySelectorAll('[translate-lang]').forEach(el => {
     el.addEventListener('click', (e) => {
       translate.translate(e.target.attributes['translate-lang'].value);
+
       document.querySelectorAll('.lang__item.active').forEach(lang =>
         lang.classList.remove('active')
-      )
+      );
+
       el.classList.add('active');
     });
   });
